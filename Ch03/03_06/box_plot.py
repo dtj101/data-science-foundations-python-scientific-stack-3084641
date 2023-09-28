8a619e894b78bd99db7a0bec8432ca72fb36ba66
dtj101 <dtj101@yahoo.com>
2023-09-28 17:05:00 -0500

    chnage to use correct style - seaborn-v0_8-whitegrid

--- Ch03/03_06/box_plot.py
+++ Ch03/03_06/box_plot.py
@@ -1,4 +1,5 @@
 # %%
+import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
@@ -8,10 +9,12 @@ df = pd.read_csv(csv_file, parse_dates=['time'])
 lat_km = 92
 lng_km = 111
 
+
 def distance(lat1, lng1, lat2, lng2):
     delta_lat = (lat1 - lat2) * lat_km
     delta_lng = (lng1 - lng2) * lng_km
     return np.hypot(delta_lat, delta_lng)
+
 
 dist = distance(
     df['lat'], df['lng'],
@@ -28,11 +31,13 @@ speed.describe()
 speed.plot.box()
 
 # %%
-import matplotlib.pyplot as plt
 
 plt.rcParams['figure.figsize'] = (10, 6)
-plt.style.use('seaborn-whitegrid')
+plt.style.use('seaborn-v0_8-whitegrid')
 speed.plot.box()
+
+# %%
+plt.style.available
 
 # %%
 speed.name = ''
