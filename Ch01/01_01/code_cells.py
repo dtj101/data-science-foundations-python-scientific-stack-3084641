88bd2506a7b989d1c988c73d5acc945a1d2debda
dtj101 <dtj101@yahoo.com>
2023-10-02 11:00:45 -0500

    Add more code

--- Ch01/01_01/code_cells.py
+++ Ch01/01_01/code_cells.py
@@ -1,13 +1,34 @@
 # %%
+import pandas as pd
+from pathlib import Path
+import numpy as np
+import matplotlib.pyplot as plt
 40 + 2
 
 # %%
 "Hi" + " there"
 
 # %%
-import matplotlib.pyplot as plt
-import numpy as np
 
 xs = np.linspace(-6, 6, 1000)
 ys = np.sin(xs)
 plt.plot(xs, ys)
+
+# %%
+
+kb = 2**10
+
+csv_file = Path('track.csv')
+csv_file.stat().st_size / kb
+
+# %%
+
+df = pd.read_csv(csv_file)
+len(df)
+
+# %%
+df.columns
+# %%
+df.info()
+# %%
+df.describe()
